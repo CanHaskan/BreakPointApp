6dbda7492f342b2a3063006b022b5ff1fe84930a
CanHaskan <canhaskan1903@hotmail.com>
2025-12-10 23:24:29 +0300

    Creating Feed, Login, and Me view controllers.

---
+++ BreakPoint/Controller/FeedVC.swift
@@ -0,0 +1,17 @@
+//
+//  FeedVC.swift
+//  BreakPoint
+//
+//  Created by Can Haskan on 10.12.2025.
+//
+
+import UIKit
+
+class FeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
---
+++ BreakPoint/Controller/MeVC.swift
@@ -0,0 +1,24 @@
+//
+//  MeVC.swift
+//  BreakPoint
+//
+//  Created by Can Haskan on 10.12.2025.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+    }
+    
+}
