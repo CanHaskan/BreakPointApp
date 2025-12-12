f3295723a8e1477b70a5b27b3b915d723a893a6c
CanHaskan <canhaskan1903@hotmail.com>
2025-12-13 00:03:28 +0300

    Uploading posts to Firestone and binding the keyboard to the UIViews  with an extension.

---
+++ BreakPoint/Controller/CreatePostVC.swift
@@ -0,0 +1,50 @@
+//
+//  CreatePostVC.swift
+//  BreakPoint
+//
+//  Created by Can Haskan on 12.12.2025.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+    
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+        sendBtn.bindToKeyboard()
+
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != "Say something here..." {
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil) { isComplete in
+                if isComplete {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendBtn.isEnabled = true
+                    print("There is an error")
+                }
+            }
+        }
+    }
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
---
+++ BreakPoint/Extensions/UIViewExt.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewExt.swift
+//  BreakPoint
+//
+//  Created by Can Haskan on 12.12.2025.
+//
+
+import UIKit
+
+
+extension UIView {
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+     @objc func keyboardWillChange(_ notification: NSNotification) {
+         
+         let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+         let curve = notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+         let beginnigFrame = (notification.userInfo![UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+         let endFrame = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+         let deltaY = endFrame.origin.y - beginnigFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIView.KeyframeAnimationOptions(rawValue: curve), animations:  {
+            self.frame.origin.y += deltaY
+        }, completion: nil )
+
+    }
+}
+
+
+
