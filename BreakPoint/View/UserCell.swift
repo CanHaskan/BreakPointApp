7930612b02d3cc974fef6b66078ccc9993625b9f
CanHaskan <canhaskan1903@hotmail.com>
2025-12-14 00:29:17 +0300

    Designing CreateGroupVC and UserCell UI

---
+++ BreakPoint/View/UserCell.swift
@@ -0,0 +1,32 @@
+//
+//  UserCell.swift
+//  BreakPoint
+//
+//  Created by Can Haskan on 13.12.2025.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected: Bool) {
+        self.profileImage.image = image
+        self.emailLbl.text = email
+        if isSelected {
+            self.checkImage.isHidden = false
+        } else {
+            self.checkImage.isHidden = true
+        }
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
